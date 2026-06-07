59cd71802a4f5b853b34df10b9798ba4d2f5e51b
Martin Morente Vargas <martin.dev45@gmail.com>
2026-06-07 20:25:46 +0200

    Update

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -7,9 +7,6 @@ cd $ANGULAR_DIR
 docker build -f setup/Dockerfile.base --target base -t angular-base .
 # Levantar el contenedor de producción
 docker compose -f setup/docker-compose.production.yml up -d --build
-sleep 10
-docker exec Angular service apache2 restart
-sleep 5
 
 echo "=== LOGS ANGULAR ==="
 
