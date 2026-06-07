72bdd275fde024df7a36db95da5b98c6840ce0f1
Martin Morente Vargas <martin.dev45@gmail.com>
2026-06-07 18:44:05 +0200

    Update

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 set -e
 set -x
-
 ANGULAR_DIR=/home/ubuntu/frontend-code
-
 cd $ANGULAR_DIR
-
 # Construir la imagen hasta el stage build
-
 docker build -f setup/Dockerfile.base --target base -t angular-base .
-
 # Levantar el contenedor de producción
 docker compose -f setup/docker-compose.production.yml up -d --build
-
+sleep 10
+docker exec Angular service apache2 restart
 sleep 5
 
 echo "=== LOGS ANGULAR ==="
+
 docker compose -f setup/docker-compose.production.yml logs angular
 
 echo "=== STATUS ==="
+
 docker compose -f setup/docker-compose.production.yml ps
-
 docker update --restart always Angular
-
-docker compose -f setup/docker-compose.production.yml restart apache angular
+docker exec Angular apachectl restart
